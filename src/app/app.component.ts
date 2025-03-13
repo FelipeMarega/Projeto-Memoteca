eeff52b1b59c6fece6981bceb6d0eb9d41adf2f9
FelipeMarega <felipe.marega00@gmail.com>
2025-03-12 22:17:28 -0300

    Criação do projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from '../app/componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  standalone: true,
+})
+export class AppComponent {
+  title = 'memoteca';
+}
