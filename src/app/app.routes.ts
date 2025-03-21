1dad9deb2232bee287050be0a83a6d52561779c9
FelipeMarega <felipe.marega00@gmail.com>
2025-03-21 19:35:53 -0300

    Rotas adicionadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+
 export const routes: Routes = [
   {
     path: 'criarPensamento',
